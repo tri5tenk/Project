c57ceec7ccb785421585f6e180d801d748d2b8b8
Dominic Evans <dominicevans@Dominics-MacBook-Pro.local>
2016-05-25 14:46:15 -0400

    newfrontend

--- newBusTracker/www/services/BusService.js
+++ newBusTracker/www/services/BusService.js
@@ -3,8 +3,8 @@ angular.module("BusTracker")
 
  routes = [
     { title: 'Airport to University Drive [56]',  name: 'Airport' },
-    { title: 'Bathsheba -> Fairchild Terminal [6]', name: 'Bathsheba' },
-    { title: 'Boscobelle -> Princess Alice Terminal [1A]', name: 'Boscobelle'},
+    { title: 'Bathsheba towards Fairchild Terminal [6]', name: 'Bathsheba' },
+    { title: 'Boscobelle towards Princess Alice Terminal [1A]', name: 'Boscobelle'},
     { title: 'Fairchild St Terminal to Princess Alice Terminal [51]', name: 'Fairchild' },
     { title: 'Bridgetown towards  Warrens [57]', name: 'Bridgetown' },
     { title: 'Fairvalley to Oistins [12A]', name: 'Fairvalley'},
