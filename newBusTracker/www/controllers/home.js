e3693a8ad2fd2b4416cd3fcee45e4db475b3c569
Rikahs <shakir4ab@hotmail.com>
2016-05-25 09:33:23 -0400

    Map Changes

--- newBusTracker/www/controllers/home.js
+++ newBusTracker/www/controllers/home.js
@@ -1,9 +1,43 @@
 
 angular.module('BusTracker')
+
 .controller("home", function($scope, Homeservice){ //dependency injection
  $scope.message = Homeservice;
 })
 
-.controller('routeCtrl', function($scope, BusService) {
+ .controller('routeCtrl', function($scope, BusService) {
   $scope.routes = BusService;
+})
+
+ .controller('MapCtrl', function($scope, $state) {
+  var options = {timeout: 10000, enableHighAccuracy: true};
+ 
+  navigator.geolocation.getCurrentPosition(function(position){
+ 
+    var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+ 
+    var mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+ 
+    $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+ 	
+ 	google.maps.event.addListenerOnce($scope.map, 'idle', function(){
+ 
+ //CREATES THE MARKER
+  var marker = new google.maps.Marker({
+      map: $scope.map,
+      animation: google.maps.Animation.DROP,
+      position: latLng
+  });      
+ 
 });
+  }, function(error){
+    console.log("Could not get location");
+  },
+  options
+  );
+});
+ 
